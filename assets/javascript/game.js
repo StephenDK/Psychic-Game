53e8c31e73d99276dc00b2c8e31f7c86137e95d7
StephenDK <stephendklein08@gmail.com>
2017-11-11 16:18:33 -0800

    Added resetGame function to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Game intro and instructions
-alert("Welcome to the abra psychic game. Guess what letter abra is thinking about?");
+//alert("Welcome to the abra psychic game. Guess what letter abra is thinking about?");
 
 
 //Array of choices for the computer.
@@ -11,25 +11,44 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 
+// This function wil reset the game counters.
+var resetGame = function() {
+	wins = 0;
+	losses = 0;
+	guessesLeft = 9;
+	document.getElementById("users-Guess").innerHTML = "";
+}
+
+
 
 //This function is run whenever the user presses a key.
 document.onkeyup = function() {
+
 
 //Users key choice
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 	console.log(userGuess);
 
-// Show users choice on index.html
-	// var playerChoice = document.getElementById("users-Guess");
-	// playerChoice.textContent = userGuess;
+// 
+// Show users selection on index.html
+	var targetSpan = document.getElementById("users-Guess");
+	
+	var newSpan = document.createElement("span");
 
-// Computers options choice
+
+	newSpan.innerHTML = userGuess;
+	targetSpan.appendChild(newSpan);
+
+
+
+// Computers choice through the options array.
 	var computerGuess = options[Math.floor(Math.random() * options.length)];
 
 	console.log(computerGuess);
 
-// If the user choice is equal to the computers choice.
+
+// If the user choice is equal to the computers choice conditions.
 	if (userGuess === computerGuess) {
 		console.log("great");
 		wins++;
@@ -40,15 +59,19 @@ document.onkeyup = function() {
 	}
 
 
-var html = "<p>Players choice: " + userGuess + "</p>" +
-"<p>Wins: " + wins + "</p>" +
+// index.html wins, losses, guessesLeft out put.
+var html = "<p>Wins: " + wins + "</p>" +
 "<p>Losses: " + losses + "</p>" +
 "<p>Guesses left: " + guessesLeft + "</p>";
 
 document.querySelector('#game').innerHTML = html;
 
+// Reset game conditional statement. 
+if (guessesLeft <= "0") {
+	resetGame();
 }
 
+}
 
 
 
